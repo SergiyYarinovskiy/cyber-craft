32c62432a412ccd5ffaf00ef0bbee30b4419d418
SergiyYarinovskiy <serezha.km@gmail.com>
2016-06-17 21:10:51 +0300

    Add example and seeds

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ### README
 
-`bundle`
-
-`rake db:create`
-
-`rake db:migrate`
+`bundle; rake db:create db:migrate db:seed`
 
 `rails s`
 
 [http://localhost:3000](http://localhost:3000)
+
+Example:
+
+![alt tag](https://github.com/SergiyYarinovskiy/cyber-craft/blob/master/public/images/example.png?raw=true)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do
+  Build.create(
+    session_id: rand(100),
+    started_by: 'admin',
+    build_created_at: Time.now.utc - rand(20).days,
+    summary_status: [Build::PASSED, Build::FAILED, Build::ERROR].sample,
+    duration: rand(1000),
+    worker_time: rand(100)
+  )
+end
